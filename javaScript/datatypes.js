e73122c7fd1804ad27dbb7154b15a60798d3355d
MD KAIF <mdkaif7625@gmail.com>
2026-09-14 19:33:32 +0530

    investigated datatypes

---
+++ javaScript/datatypes.js
@@ -0,0 +1,20 @@
+// one committee formed to create standards for dev to follow atleast - ECMAscript
+
+Number
+BigInt
+Boolean
+String
+null
+undefined
+Symbol
+
+
+console.log(typeof Number)
+console.log(typeof BigInt)
+console.log(typeof Boolean)
+console.log(typeof String)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof Symbol)
+
+
