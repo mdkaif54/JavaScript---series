0d8e0f46949bc4bea5cfb61d96f8aa99869cc567
MD KAIF <mdkaif7625@gmail.com>
2026-09-18 09:37:24 +0530

    investigated dts

--- javaScript/dtComparisions.js
+++ javaScript/dtComparisions.js
@@ -18,6 +18,44 @@
 // console.log(null <= 3)
 
 
-console.log(undefined == 3)
-console.log(undefined >= 3)
-console.log(undefined <= 3)
+// console.log(undefined == 3)
+// console.log(undefined >= 3)
+// console.log(undefined <= 3)
+
+
+//  Primitive types: Number String Boolean BigInt Symbol undefined null
+const numberValue = 34
+const stringValue = "Thirty four"
+const boolValue = true
+const symbolValue0 = Symbol('thirty')
+const symbolValue1 = Symbol('thirty')
+const nullValue = null
+const undefinedValue = undefined
+
+//  console.log(typeof (numberValue))
+//  console.log(typeof (stringValue)) 
+//  console.log(typeof (boolValue))
+//  console.log(typeof (symbolValue0))
+//  console.log(typeof (symbolValue1))
+//  console.log(typeof (nullValue))
+//  console.log(typeof (undefinedValue))
+
+
+//  Non-Primitive types: Array, Object, Function
+
+const pokedex = ['pikachu', 'charlizard', 'snorlax']
+
+let someObject = {
+    name: "Raichu",
+    height: 3,
+    master: "Ash"
+}
+
+let user = function() {
+    console.log(isLoggedIn = true)
+}
+
+
+console.log(typeof(pokedex))
+console.log(typeof(someObject))
+console.log(typeof(user))
