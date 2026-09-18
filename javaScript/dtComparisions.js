55320f9102517ee9e8766cbb47e42e77abc7cbd6
MD KAIF <mdkaif7625@gmail.com>
2026-09-18 08:58:59 +0530

    investigated datatypestConversions

---
+++ javaScript/dtComparisions.js
@@ -0,0 +1,23 @@
+// console.log(2 > 2)
+// console.log(2 < 2)
+// console.log(2 >= 2)
+// console.log(2 <= 2)
+// console.log(2 == 2)
+// console.log(2 === 2)
+
+// console.log("2" > 2)
+// console.log("2" < 2)
+// console.log("2" >= 2)
+// console.log("2" <= 2)
+// console.log("2" == 2)
+// console.log("2" === 2)
+
+
+// console.log(null == 3)
+// console.log(null >= 3)       == & ">= <= < >" works differently - It converts null into numbers
+// console.log(null <= 3)
+
+
+console.log(undefined == 3)
+console.log(undefined >= 3)
+console.log(undefined <= 3)
